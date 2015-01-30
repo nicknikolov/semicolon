55db431b2a4d937dcbb9396dd342f89997a6433a
nicknikolov <nick.nikolov.music@gmail.com>
2015-01-30 19:59:00 +0000

    Change JavaScript editor from Ace to Codemirror, plus simple compilation with Processing.js

--- semicolon/javascript/main.js
+++ semicolon/javascript/main.js
@@ -1,9 +1,33 @@
+var editor = CodeMirror.fromTextArea(document.getElementById('editor'), {
+    mode: "text/x-java",
+    lineNumbers: true,
+    theme: 'solarized',
+    smartIndent: true,
+    indentUnit: 2,
+    tabSize: 2,
+});
+var canvas = document.getElementById('pjs');
 
-var editor = ace.edit("editor");
+editor.on("change", function(e) {
 
-editor.setTheme("ace/theme/processing");
-editor.getSession().setMode("ace/mode/java");
-editor.getSession().setUseWrapMode(true);
-editor.setFontSize(15);
+    try {
+        canvas = createCanvas();
+        var sketch = Processing.compile(editor.getValue());
+        instance = new Processing(canvas, sketch);
+    } catch (e) {
+        console.log(e);
+        output.value = "Processing.js error:\n" + e.toString();
+    }
+});
 
-console.log(editor.getSession().getDocument().getAllLines());
+function createCanvas() {
+    // Make a new canvas, in case we're switching from 2D to 3D contexts.
+    var container = document.getElementById('sketch');
+    var sketch = document.getElementById('pjs');
+    container.removeChild(sketch);
+    sketch = document.createElement('canvas');
+    sketch.id = 'pjs';
+    container.appendChild(sketch);
+    return sketch;
+}
+
--- semicolon/test.pde
+++
@@ -1,3 +0,0 @@
-void foo() {
-	String greeting = "hello";
-}
