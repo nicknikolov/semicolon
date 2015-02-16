b35ff48ac41d165d0f57099ee5ca1fc7ca1f1d39
nicknikolov <nick.nikolov.music@gmail.com>
2015-02-16 19:15:21 +0000

    Saner gestures and saner options for them

--- semicolon/javascript/main.js
+++ semicolon/javascript/main.js
@@ -17,29 +17,75 @@ if (Hammer.defaults.cssProps.hasOwnProperty("userSelect")) {
 }
 
 var editorDiv = document.getElementsByClassName('CodeMirror')[0];
-var mc = new Hammer.Manager(editorDiv, {touchAction: 'auto'});
+var mc = new Hammer.Manager(editorDiv, {
+    // options
+});
 var canvas = document.getElementById('pjs');
 var sketchVisible = true;
 var selectedSketch = 'Default Sketch';
 var instance;
 
 // add gestures
-mc.add( new Hammer.Tap({ event: 'doubletap', taps: 2 }) );
-
-mc.add( new Hammer.Swipe({
+var doubleswipeLeft =  new Hammer.Swipe({
     event: 'doubleswipe-left',
     pointers: 2,
-    direction: Hammer.DIRECTION_LEFT
-}));
-mc.add( new Hammer.Swipe({
+    direction: Hammer.DIRECTION_LEFT,
+    threshhold: 3,
+    velocity: 0.1
+});
+
+var swipeLeft = new Hammer.Swipe({
+    event: 'swipe-left',
+    pointers: 1,
+    direction: Hammer.DIRECTION_LEFT,
+    threshhold: 3,
+    velocity: 0.1
+});
+
+var doubleswipeRight = new Hammer.Swipe({
     event: 'doubleswipe-right',
     pointers: 2,
-    direction: Hammer.DIRECTION_RIGHT
-}));
+    direction: Hammer.DIRECTION_RIGHT,
+    threshhold: 3,
+    velocity: 0.1
+});
+
+var swipeRight = new Hammer.Swipe({
+    event: 'swipe-right',
+    pointers: 1,
+    direction: Hammer.DIRECTION_RIGHT,
+    threshhold: 3,
+    velocity: 0.1
+});
+
+var doubletap = new Hammer.Tap({
+    event: 'doubletap',
+    taps: 2,
+    pointers: 1
+});
+
+
+var tripletap = new Hammer.Tap({
+    event: 'tripletap',
+    taps: 3,
+    pointers: 1
+});
+
+mc.add([doubletap,
+        tripletap,
+        doubleswipeLeft,
+        swipeLeft,
+        swipeRight,
+        doubleswipeRight
+]);
+
+tripletap.recognizeWith(doubletap);
+doubletap.requireFailure(tripletap);
 
 mc.on("doubletap", function(ev) {
-    // delete line at cursor on doubletab
-    editor.execCommand('deleteLine');
+    // kill word
+    editor.execCommand('delWordBefore');
+    editor.execCommand('delWordAfter');
 });
 
 mc.on('doubleswipe-left', function(e) {
@@ -50,6 +96,25 @@ mc.on('doubleswipe-left', function(e) {
 mc.on('doubleswipe-right', function(e) {
     // doubleswipe to the left undo's the last action
     editor.execCommand('redo');
+});
+
+mc.on('swipe-left', function(e) {
+    // go one word left
+    editor.execCommand('goWordLeft');
+});
+
+mc.on('swipe-right', function(e) {
+    // go one word right
+    editor.execCommand('goWordRight');
+});
+
+
+mc.on('tripletap', function(e) {
+    // add ; after cursors
+    var doc = editor.getDoc();
+    var cursor = doc.getCursor();
+    doc.replaceRange(';', cursor);
+
 });
 
 // rerender Processing sketch on code change
