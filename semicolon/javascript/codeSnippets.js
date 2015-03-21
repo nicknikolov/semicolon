ffaed2aef74a6e51784b08423beba4fcbd03c1e8
nicknikolov <nick.nikolov.music@gmail.com>
2015-03-21 19:09:52 +0000

    Add smart code snippets for for and while loop. When you select something and insert a loop, it asks you for counter variable and loop amount, and wraps the loop around the selected code. If there is nothing selected it inserts it at cursor position

--- semicolon/javascript/codeSnippets.js
+++ semicolon/javascript/codeSnippets.js
@@ -1,22 +1,111 @@
 function toggleCodeSnippets() {
 
     var codeSnippetsDiv = document.getElementById('code-snippets');
-    codeSnippetsDiv.innerHTML = '';
 
     if (codeSnippetsDiv.style.display === 'none') codeSnippetsDiv.style.display = 'inline';
     else { codeSnippetsDiv.style.display = 'none'; return; }
 
+    var ul = document.getElementById('snippetsList');
+    console.log(ul);
+    ul.innerHTML = '';
+
+    var forLi = document.createElement('li');
     var forLoop = document.createElement('div');
     forLoop.setAttribute('class', 'snippet');
-    forLoop.setAttribute('onclick', 'insertForLoop()');
     forLoop.innerHTML = 'for loop';
-    codeSnippetsDiv.appendChild(forLoop);
+    forLi.appendChild(forLoop);
+    forLi.setAttribute('onclick', 'insertForLoop()');
+    ul.appendChild(forLi);
+
+    var whileLi = document.createElement('li');
+    var whileLoop = document.createElement('div');
+    whileLoop.setAttribute('class', 'snippet');
+    whileLoop.innerHTML = 'while loop';
+    whileLi.appendChild(whileLoop);
+    whileLi.setAttribute('onclick', 'insertWhileLoop()');
+    ul.appendChild(whileLi);
 
 }
 
 function insertForLoop() {
-    var doc = editor.getDoc();
-    var cursor = doc.getCursor();
-    editor.replaceRange('for (var i=0; i<3; i++){}', cursor);
+    document.activeElement.blur(); // hide keyboard hack
+    if (lastSelection.code){
+        var doc = editor.getDoc();
+        var start = doc.getCursor(true);
+        var i = prompt("count variable:", "i");
+        if (!i) {
+            toggleCodeSnippets();
+            return;
+        }
+        var loops = prompt("loop amount:", "3");
+        if (!loops) {
+            toggleCodeSnippets();
+            return;
+        }
+        editor.replaceRange('', lastSelection.start, lastSelection.end);
+        editor.replaceRange(
+                "for (var "+i+"=0; "+i+"<"+loops+"; "+i+"++) {\n "
+                + lastSelection.code + "\n}", lastSelection.start
+        );
+    } else {
+        var doc = editor.getDoc();
+        var start = doc.getCursor();
+        var i = prompt("count variable:", "i");
+        if (!i) {
+            toggleCodeSnippets();
+            return;
+        }
+        var loops = prompt("loop amount:", "3");
+        if (!loops) {
+            toggleCodeSnippets();
+            return;
+        }
+        editor.replaceRange(
+                "for (var "+i+"=0; "+i+"<"+loops+"; "+i+"++) {}", start
+        );
+    }
+    editor.execCommand('selectAll');
+    editor.execCommand('indentAuto');
+    toggleCodeSnippets();
+}
+
+function insertWhileLoop() {
+    document.activeElement.blur(); // hide keyboard hack
+    if (lastSelection.code) {
+        var i = prompt("count variable:", "i");
+        if (!i) {
+            toggleCodeSnippets();
+            return;
+        }
+        var loops = prompt("loop amount:", "3");
+        if (!loops) {
+            toggleCodeSnippets();
+            return;
+        }
+        editor.replaceRange('', lastSelection.start, lastSelection.end);
+        editor.replaceRange(
+                "var " + i + " = 0;\nwhile ("+i+"<"+loops+") {\n "
+                + lastSelection.code + "\n"+i+"++;\n}", lastSelection.start
+        );
+    } else {
+        var doc = editor.getDoc();
+        var start = doc.getCursor();
+        var i = prompt("count variable:", "i");
+        if (!i) {
+            toggleCodeSnippets();
+            return;
+        }
+        var loops = prompt("loop amount:", "3");
+        if (!loops) {
+            toggleCodeSnippets();
+            return;
+        }
+        editor.replaceRange(
+                "var " + i + " = 0;\nwhile ("+i+"<"+loops+") {"
+                + "\n"+i+"++;\n}", start
+        );
+    }
+    editor.execCommand('selectAll');
+    editor.execCommand('indentAuto');
     toggleCodeSnippets();
 }
--- semicolon/javascript/editor.js
+++ semicolon/javascript/editor.js
@@ -14,6 +14,27 @@ var editor = CodeMirror.fromTextArea(document.getElementById('editor'), {
 // rerender Processing sketch on code change
 editor.on('change', renderSketch);
 
+var lastSelection = { start: 0, end: 0, code: ''};
+var lastOneWasNothingToo = false;
+
+editor.on('cursorActivity', function() {
+    // a bit of a hack but, keep last marked
+    // chunk of text so I can do stuff with it
+    // e.g. wrap for or while loops around it
+    var doc = editor.getDoc();
+    var start = doc.getCursor(true);
+    var end = doc.getCursor(false);
+    var newSelection = doc.getRange(start, end);
+    if (newSelection || lastOneWasNothingToo) {
+        lastSelection.start = start;
+        lastSelection.end = end;
+        lastSelection.code = newSelection;
+        lastOneWasNothingToo = false;
+    } else {
+        lastOneWasNothingToo = true;
+    }
+});
+
 editor.on('inputRead', function(e) {
     CodeMirror.showHint(editor, CodeMirror.hint.auto,
             { 'completeOnSingleClick': true });
